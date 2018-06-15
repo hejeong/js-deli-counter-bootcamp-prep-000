e0dd333b98aa9155441d93d50859b23e1f5043f6
Jonathan Hong <jonhong777@gmail.com>
2018-06-15 16:34:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(line , name){
   var placeInLine = line.length
   return `Welcome, ${name}. You are number ${placeInLine} in line.`
 }
+
+function nowServing(line){
+  if(line.length === 0){
+    return "There is nobody waiting to be served!"
+  }else{
+    var nextInLine = line.shift()
+    return `Currently serving ${nextInLine}`
+  }
+}
