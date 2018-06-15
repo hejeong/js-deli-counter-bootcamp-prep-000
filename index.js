e39ff9cb11c1b011577512260f1d44ea87818be2
Jonathan Hong <jonhong777@gmail.com>
2018-06-15 16:32:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line , name){
   line.push(name)
   var placeInLine = line.length
-  return "Welcome, ${name}. You are number ${placeInLine} in line."
+  return `Welcome, ${name}. You are number ${placeInLine} in line.`
 }
